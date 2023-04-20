8a6129865b68328d0f54e09d0adb8770382e54c6
Marwa93mira <75796895+Marwa93mira@users.noreply.github.com>
2023-04-20 13:49:39 +0200

    first-version
    first version

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Main from "./components/Main";
+import "./components/style.css";
+function App() {
+  return (
+    <>
+      <Main />
+    </>
+  );
+}
+
+export default App;
